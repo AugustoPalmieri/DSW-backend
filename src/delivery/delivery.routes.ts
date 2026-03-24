f4103c7f3d4b14aa6cbcfe19a7fde2cb7fe7d2dc
Augusto <augustopalmieri6@gmail.com>
2026-03-24 17:09:57 -0300

    + CORRECCIONES

--- src/delivery/delivery.routes.ts
+++ src/delivery/delivery.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { findLatest, add } from "./delivery.controler.js";
 import { adminMiddleware } from "../middleware/auth.js";
 
-console.log("delivery.routes cargado");
 export const deliveryRouter = Router();
 
 deliveryRouter.get("/", findLatest);                    
